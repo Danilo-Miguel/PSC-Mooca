bbc3be09fdf2c77d25ea6095f08fdbf8e0e14fb0
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2024-05-21 11:02:56 -0300

    Create ConnectionFactory.java

---
+++ ConnectionFactory.java
@@ -0,0 +1,46 @@
+package com.mycompany.bancodedados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private String usuario = "root";
+    private String senha = "Belchior*14142418";
+    private String host = "localhost";
+    private String porta = "3306";
+    private String bd = "psc_bua";
+    //private String timezone = "America/Sao_Paulo"; // Adiciona o fuso horário
+
+
+    public Connection obtemConexao() {
+        try {
+            
+            Connection conectar = DriverManager.getConnection(
+                "jdbc:mysql://"+host+":"+porta+"/"+ bd,
+                usuario,
+                senha
+            );
+            if (conectar != null) {
+                System.out.println("Conexão estabelecida com sucesso!");
+            }
+            return conectar;
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar estabelecer conexão com o banco de dados:");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.obtemConexao();
+        
+        if (connection != null) {
+            System.out.println("Conexão testada e aprovada!");
+        } else {
+            System.out.println("Falha na conexão!");
+        }
+    }
+}
